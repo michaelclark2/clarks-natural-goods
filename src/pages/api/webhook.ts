05ef982a369b5be11ce8cf6af1777fb11eac6ea2
Michael Clark <mc2.dev@proton.me>
2024-10-23 10:26:30 -0500

    move page to api endpoint

---
+++ src/pages/api/webhook.ts
@@ -0,0 +1,7 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.json();
+
+  return new Response(JSON.stringify({ message: data }));
+};
