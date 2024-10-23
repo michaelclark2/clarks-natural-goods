68cb33e1b4869cfbfd01b4181601e7685a2faa73
Michael Clark <mc2.dev@proton.me>
2024-10-23 10:50:25 -0500

    just a get

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,5 +1,8 @@
 import type { APIRoute } from "astro";
 
+export const GET: APIRoute = async ({ params, request }) => {
+  return new Response(JSON.stringify({ message: "You got GOT!" }));
+};
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.json();
 
