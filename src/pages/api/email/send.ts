edc8e6824fbed93ab39b79500b18aec9e1e1b4a1
Michael Clark <33385949+michaelclark2@users.noreply.github.com>
2024-11-15 17:20:57 -0600

    Add reCAPTCHA and send confirmation and welcome emails with sendgrid (#4)
    * added email route, creates encoded key that expires in 3 days
    
    * moved footer to Layout
    
    * made padding consistent in other form elements, changed way form submit changes dom
    
    * added sendgrid, creates email template, sends email
    
    * refactor redis calls to transaction
    
    * update comments
    
    * added confirm page to read param, refactor Layout
    
    * adds customer to shopify if deleted from redis
    
    * send dev urls in emails
    
    * updated submitted thank you message
    
    * move public access token to env var
    
    * added more AOS elements, whitespace
    
    * added more spacing
    
    * use admin api instead of storefront api to create customers
    
    * removed shopify, sends email using dynamic template, adds email to confirmedEmail set
    
    * ordered SADD call properly
    
    * added console log
    
    * console log errors
    
    * await sendgrid call
    
    * removed old email template
    
    * sends welcome email on confirm
    
    * cleanup console logs
    
    * await sendgrid, log errors
    
    * added unsub links
    
    * added recaptcha to subscribe form
    
    * added cors to site.webmanifest
    
    * revert
    
    * revert revert
    
    * await fetch to send email
    
    * move email/send to api/email/send
    
    * delays welcome email by one day
    
    * better spacing on intro section

---
+++ src/pages/api/email/send.ts
@@ -0,0 +1,67 @@
+import type { APIRoute } from "astro";
+import sendGrid from "@sendgrid/mail";
+sendGrid.setApiKey(import.meta.env.SENDGRID_API_KEY);
+
+export const POST: APIRoute = async ({ request }: { request: Request }) => {
+  const data = await request.json();
+
+  const emailToAdd = data.email;
+
+  if (!emailToAdd) {
+    return new Response(null, {
+      status: 400,
+    });
+  }
+
+  const emailEncoded = btoa(emailToAdd.toString());
+
+  const addEmailTransactionResponses: { result: number }[] = await fetch(
+    import.meta.env.KV_REST_API_URL + "/multi-exec",
+    {
+      // expire in 3 days
+      body: `[
+          ["HSET", "${emailEncoded}", "email", "${emailToAdd}"],
+          ["EXPIRE", "${emailEncoded}", "259200"]
+        ]`,
+      headers: {
+        Authorization: "Bearer " + import.meta.env.KV_REST_API_TOKEN,
+      },
+      method: "POST",
+    }
+  ).then((r) => r.json());
+
+  const shouldSendEmail = addEmailTransactionResponses.every(
+    (response) => response.result === 1
+  );
+
+  if (shouldSendEmail) {
+    const confirmUrl =
+      `${import.meta.env.VERCEL_ENV === "preview" ? import.meta.env.VERCEL_BRANCH_URL : import.meta.env.SITE}/email/confirm/` +
+      encodeURI(emailEncoded);
+
+    const message = {
+      to: emailToAdd,
+      from: import.meta.env.EMAIL_ADDRESS,
+      templateId: "d-33ed46c01674433683c6b767cfe0e087",
+      dynamicTemplateData: {
+        confirm_url: confirmUrl,
+      },
+      asm: {
+        groupId: 25320,
+      },
+    };
+
+    await sendGrid
+      .send(message)
+      .then((response) => {
+        console.log(response[0].statusCode);
+        console.log(response[0].headers);
+      })
+      .catch(console.error);
+  } else {
+    return new Response(null, { status: 400 });
+  }
+  return new Response(null, {
+    status: 204,
+  });
+};
